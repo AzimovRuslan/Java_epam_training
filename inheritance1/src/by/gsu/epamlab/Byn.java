91a6f83bf0aac4b4781863aec601e7865c42abd1
ruslan_assimov <azimov_2001@bk.ru>
2021-03-23 18:51:27 +0300

    fix

--- inheritance1/src/by/gsu/epamlab/Byn.java
+++ inheritance1/src/by/gsu/epamlab/Byn.java
@@ -57,27 +57,5 @@ public class Byn implements Comparable<Byn>{
         return financialValue - byn.financialValue;
     }
 
-    public enum RoundMethod {
-        ROUND {
-            double roundFunction(double d){
-                return Math.round(d);
-            }
-        },
-        FLOOR {
-            double roundFunction(double d){
-                return Math.floor(d);
-            }
-        },
-        CEIL {
-            double roundFunction(double d){
-                return Math.ceil(d);
-            }
-        };
-        abstract double roundFunction(double value);
 
-        public int round(double value, int d){
-            int[] tenPow = {1, 2, 3, 4, 5, 6, 7, 8};
-            return (int)roundFunction(value / Math.pow(10, tenPow[d]) * Math.pow(10, tenPow[d]));
-        }
-    }
 }
