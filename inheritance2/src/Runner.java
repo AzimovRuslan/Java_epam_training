112ebf227cb8a10ae477400dcc2fb8791fb29a37
ruslan_assimov <azimov_2001@bk.ru>
2021-03-24 23:07:35 +0300

    fsd

--- inheritance2/src/Runner.java
+++ inheritance2/src/Runner.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Runner {
+import by.gsu.epamlab.*;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args){
+        final Product product = new Product("Milk", new Byn(180));
+        AbstractPurchase[] abstractPurchases = {new PriceDiscountPurchase(product, 9, new Byn(20)),
+                new PriceDiscountPurchase(product, 3, new Byn(20)),
+                new PercentDiscountPurchase(product, 3, 5.125),
+                new PercentDiscountPurchase(product, 6, 6.2),
+                new TransportPurchase(product, 10, 200),
+                new TransportPurchase(product, 8, 180)};
+
+        Arrays.sort(abstractPurchases);
+
+        for(AbstractPurchase abstractPurchase : abstractPurchases){
+            System.out.println(abstractPurchase);
+        }
+    }
 }
--- inheritance2/src/by/gsu/epamlab/PriceDiscountPurchase.java
+++ inheritance2/src/by/gsu/epamlab/PriceDiscountPurchase.java
@@ -1,2 +1,20 @@
-package by.gsu.epamlab;public class PriceDiscountPurchase {
+package by.gsu.epamlab;
+
+public class PriceDiscountPurchase extends AbstractPurchase{
+    private final Byn discount;
+
+    public PriceDiscountPurchase(Product product, int number, Byn discount) {
+        super(product, number);
+        this.discount = discount;
+    }
+
+    @Override
+    protected Byn getFullCost(Byn originalCost) {
+        return originalCost.sub(discount.mul(getNumber()));
+    }
+
+    @Override
+    public int compareTo(AbstractPurchase o) {
+        return this.getCost().getKopecks() - o.getCost().getKopecks();
+    }
 }
