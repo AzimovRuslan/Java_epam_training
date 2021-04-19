d54b1b11de4c849f9847c42eaff45175a62eab48
ruslan_assimov <azimov_2001@bk.ru>
2021-04-19 11:04:00 +0300

    string2

---
+++ string2/src/Runner.java
@@ -0,0 +1,47 @@
+import com.sun.jdi.DoubleValue;
+
+import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Runner {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        try {
+            ResourceBundle rb = ResourceBundle.getBundle("text");
+            Enumeration<String> keys = rb.getKeys();
+            String key;
+            double sum = 0;
+            List<String> findValues = new ArrayList<>();
+            int error = 0;
+
+            Pattern indexPattern = Pattern.compile("^index(.*)");
+            Pattern numberPattern = Pattern.compile("[1-9]\\d*");
+
+            while (keys.hasMoreElements()) {
+                key = keys.nextElement();
+                String j = rb.getString(key).trim();
+                Matcher indexMatcher = indexPattern.matcher(key);
+                if (indexMatcher.matches()) {
+                    String i = indexMatcher.group(1).trim();
+                    if (numberPattern.matcher(i).matches() && numberPattern.matcher(j).matches()) {
+                        String value = "value" + i + j;
+                        try {
+                            sum += Double.parseDouble(rb.getString(value).trim());
+                        } catch (NumberFormatException | MissingResourceException e) {
+                            error++;
+                        }
+                    } else {
+                        error++;
+                    }
+                }
+            }
+            System.out.println("sum = " + sum);
+            System.out.println("error-lines = " + error);
+        } catch (MissingResourceException e) {
+            System.out.println("File not found");
+        }
+    }
+}
