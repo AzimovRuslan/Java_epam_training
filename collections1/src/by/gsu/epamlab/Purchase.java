ff783830529c1d4e41138881924bdc96c3992034
ruslan_assimov <azimov_2001@bk.ru>
2021-07-16 20:25:06 +0300

    collections1

---
+++ collections1/src/by/gsu/epamlab/Purchase.java
@@ -0,0 +1,50 @@
+package by.gsu.epamlab;
+
+import java.util.Scanner;
+
+public class Purchase {
+    private String name;
+    private Byn price;
+    private int number;
+
+    public Purchase(String name, Byn price, int number) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
+
+    public Purchase() {
+        this(null, new Byn(0), 0);
+    }
+
+    public Purchase(Scanner sc) {
+        name = sc.next();
+        price = new Byn(sc.nextInt());
+        number = sc.nextInt();
+    }
+
+    public Byn getCost() {
+        return new Byn(price).mul(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    protected String fieldsToString() {
+        return name + ";" + price + ";" + number;
+    }
+
+    @Override
+    public String toString() {
+        return fieldsToString() + ";" + getCost();
+    }
+//
+//    @Override
+//    public boolean equals(Object object) {
+//        if (this == object) return true;
+//        if (object == null ) return false;
+//        if(!(object instanceof Purchase)) return false;Purchase purchase = (Purchase) object;
+//        return Objects.equals(name, purchase.name) && Objects.equals(price, purchase.price);
+//    }
+}
