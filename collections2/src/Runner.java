29db8f93c1ab04c4e10c6ccf36f78bc36e9792e6
ruslan_assimov <azimov_2001@bk.ru>
2021-09-22 13:00:50 +0300

    collections2_3

--- collections2/src/Runner.java
+++ collections2/src/Runner.java
@@ -30,7 +30,7 @@ public class Runner {
             Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
                 @Override
                 public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                    return 0;
+                    return o2.getValue() - o1.getValue();
                 }
             });
 
