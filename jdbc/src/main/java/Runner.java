81225601cf0ef77751be1473d6700e18e00cab73
ruslan_assimov <azimov_2001@bk.ru>
2021-10-06 19:38:58 +0300

    jdbc1_2

--- jdbc/src/main/java/Runner.java
+++ jdbc/src/main/java/Runner.java
@@ -7,51 +7,33 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
+
         try {
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
             PreparedStatement preparedStatement = null;
-            Connection connection = null;
-            Statement statement = null;
             ResultSet resultSet = null;
-            try {
-                connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD);
-                statement = connection.createStatement();
+            try (Connection connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD); Statement statement = connection.createStatement();) {
+                statement.executeUpdate(Constants.DELETE_FROM_FREQUENCIES);
                 resultSet = statement.executeQuery(Constants.SELECT_FROM_COORDINATES);
 
                 List<Frequence> frequencies = new ArrayList<>();
 
                 while (resultSet.next()) {
-                    int len = resultSet.getInt(1);
-                    int num = resultSet.getInt(2);
-                    frequencies.add(new Frequence(len, num));
+                    frequencies.add(new Frequence(resultSet.getInt(Constants.LEN_IND), resultSet.getInt(Constants.NUM_IND)));
                 }
 
-                preparedStatement = connection.prepareStatement(Constants.DELETE_FROM_FREQUENCIES);
-                preparedStatement.execute();
-
+                preparedStatement = connection.prepareStatement(Constants.INSERT_INTO_FREQUENCIES);
                 for (Frequence frequence : frequencies) {
-                    preparedStatement = connection.prepareStatement(Constants.INSERT_INTO_FREQUENCIES);
-                    preparedStatement.setInt(Constants.FIRST_ELEMENT, frequence.getLen());
-                    preparedStatement.setInt(Constants.SECOND_ELEMENT, frequence.getNum());
-                    preparedStatement.execute();
+                    preparedStatement.setInt(Constants.LEN_IND, frequence.getLen());
+                    preparedStatement.setInt(Constants.NUM_IND, frequence.getNum());
+                    preparedStatement.addBatch();
                 }
+                preparedStatement.executeBatch();
 
-                ResultSet resultSetFrequencies = statement.executeQuery(Constants.SELECT_FROM_FREQUENCIES_WHERE_LEN_MORE_NUM);
-                while(resultSetFrequencies.next()) {
-                    System.out.println(resultSetFrequencies.getInt(Constants.FIRST_ELEMENT) + Constants.DELIMITER + resultSetFrequencies.getInt(Constants.SECOND_ELEMENT));
-                }
-            } finally {
-                if (connection != null) {
-                    connection.close();
-                }
-
-                if (statement != null) {
-                    statement.close();
-                }
-
-                if (resultSet != null && !resultSet.isClosed()) {
-                    resultSet.close();
+                resultSet = statement.executeQuery(Constants.SELECT_FROM_FREQUENCIES_WHERE_LEN_MORE_NUM);
+                while(resultSet.next()) {
+                    System.out.println(resultSet.getInt(Constants.LEN_IND) + Constants.DELIMITER + resultSet.getInt(Constants.NUM_IND));
                 }
             }
         } catch (SQLException e) {
--- jdbc/src/main/java/by/gsu/epamlab/Constants.java
+++ jdbc/src/main/java/by/gsu/epamlab/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
     public static String INSERT_INTO_FREQUENCIES = "INSERT INTO frequencies VALUES (?, ?)";
     public static String DELETE_FROM_FREQUENCIES = "delete from frequencies";
     public static String SELECT_FROM_FREQUENCIES_WHERE_LEN_MORE_NUM = "SELECT * FROM frequencies WHERE len>num";
-    public static int FIRST_ELEMENT = 1;
-    public static int SECOND_ELEMENT = 2;
+    public static int LEN_IND = 1;
+    public static int NUM_IND = 2;
     public static String FAILED_LOAD_DRIVER = "Failed to load class driver";
     public static String URL = "jdbc:mysql://localhost:3306/segments";
     public static String USER = "root";
