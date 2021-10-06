16f0f2f78488aa8879903be2fa560ba76d51fe70
ruslan_assimov <azimov_2001@bk.ru>
2021-10-06 19:57:51 +0300

    jdbc

--- jdbc/src/main/java/Runner.java
+++ jdbc/src/main/java/Runner.java
@@ -7,23 +7,19 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-
         try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            PreparedStatement preparedStatement = null;
-            ResultSet resultSet = null;
-            try (Connection connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD); Statement statement = connection.createStatement();) {
+            try (Connection connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD); Statement statement = connection.createStatement();
+                 PreparedStatement preparedStatement = connection.prepareStatement(Constants.INSERT_INTO_FREQUENCIES);) {
+                
                 statement.executeUpdate(Constants.DELETE_FROM_FREQUENCIES);
-                resultSet = statement.executeQuery(Constants.SELECT_FROM_COORDINATES);
 
                 List<Frequence> frequencies = new ArrayList<>();
-
-                while (resultSet.next()) {
-                    frequencies.add(new Frequence(resultSet.getInt(Constants.LEN_IND), resultSet.getInt(Constants.NUM_IND)));
+                try (ResultSet resultSet = statement.executeQuery(Constants.SELECT_FROM_COORDINATES);) {
+                    while (resultSet.next()) {
+                        frequencies.add(new Frequence(resultSet.getInt(Constants.LEN_IND), resultSet.getInt(Constants.NUM_IND)));
+                    }
                 }
 
-                preparedStatement = connection.prepareStatement(Constants.INSERT_INTO_FREQUENCIES);
                 for (Frequence frequence : frequencies) {
                     preparedStatement.setInt(Constants.LEN_IND, frequence.getLen());
                     preparedStatement.setInt(Constants.NUM_IND, frequence.getNum());
@@ -31,9 +27,10 @@ public class Runner {
                 }
                 preparedStatement.executeBatch();
 
-                resultSet = statement.executeQuery(Constants.SELECT_FROM_FREQUENCIES_WHERE_LEN_MORE_NUM);
-                while(resultSet.next()) {
-                    System.out.println(resultSet.getInt(Constants.LEN_IND) + Constants.DELIMITER + resultSet.getInt(Constants.NUM_IND));
+                try (ResultSet resultSet = statement.executeQuery(Constants.SELECT_FROM_FREQUENCIES_WHERE_LEN_MORE_NUM);) {
+                    while(resultSet.next()) {
+                        System.out.println(resultSet.getInt(Constants.LEN_IND) + Constants.DELIMITER + resultSet.getInt(Constants.NUM_IND));
+                    }
                 }
             }
         } catch (SQLException e) {
