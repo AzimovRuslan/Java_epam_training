f015a34501789ca7e6c541c4a5ed7e98bda7eb40
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2021-10-10 23:50:23 +0300

    Add files via upload

---
+++ src/constants/Constants.java
@@ -0,0 +1,20 @@
+package constants;
+
+import java.text.SimpleDateFormat;
+
+public class Constants {
+    public static String FILENAME = "src/results.xml";
+    public static int NAME_IND = 0;
+    public static int DATE_IND = 1;
+    public static int MARK_IND = 2;
+    public static String DELIMITER = ";";
+    public static String POINT = ".";
+    public static String GET_DATE_FORMAT = "yyyy-MM-dd";
+    public static String SET_DATE_FORMAT = "dd.MM.yyyy";
+    public static int DENOMINATOR_FOR_MARK = 10;
+    public static SimpleDateFormat IN_DATE_FORMAT = new SimpleDateFormat(Constants.GET_DATE_FORMAT);
+    public static SimpleDateFormat OUT_DATE_FORMAT = new SimpleDateFormat(Constants.SET_DATE_FORMAT);
+    public static String ERROR_SAX_PARSER = "Error SAX parse";
+    public static String ERROR_IO = "Error I/O stream";
+
+}
