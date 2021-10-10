2abf4c9b0982f5e5febaf3a524c1785866e24a5e
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2021-10-10 20:57:52 +0300

    Add files via upload

---
+++ src/by/gsu/epamlab/Constants.java
@@ -0,0 +1,17 @@
+package by.gsu.epamlab;
+
+public class Constants {
+    public static String FILENAME = "src/results.xml";
+    public static int NAME_IND = 0;
+    public static int DATE_IND = 1;
+    public static int MARK_IND = 2;
+    public static String DELIMITER = ";";
+    public static String POINT = ".";
+    public static String DASH = "-";
+    public static int FIRST_ELEMENT = 0;
+    public static int SECOND_ELEMENT = 1;
+    public static int THIRD_ELEMENT = 2;
+    public static String SPLIT_POINT = "\\.";
+    public static String DATE_FORMAT = "dd.MM.yyyy";
+    public static int DENOMINATOR_FOR_MARK = 10;
+}
