d6fbccc3d8250586084fe034c92df132699f2245
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2021-10-10 23:26:05 +0300

    Add files via upload

---
+++ src/beans/Result.java
@@ -0,0 +1,77 @@
+package beans;
+
+import constants.Constants;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Result {
+    private String login;
+    private String test;
+    private Date date;
+    private int mark;
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(Constants.DATE_FORMAT);
+    private static final SimpleDateFormat OUT_DATE_FORMAT = new SimpleDateFormat(Constants.OUT_DATE_FORMAT);
+
+    public Result(String login, String test, Date date, int mark) {
+        this.login = login;
+        this.test = test;
+        this.date = date;
+        this.mark = mark;
+    }
+
+    public Result() {
+        this("", "", new Date(0), 0);
+    }
+
+    @Override
+    public String toString() {
+        return login + Constants.DELIMITER + test + Constants.DELIMITER + getStringDate() + Constants.DELIMITER + getStringMark();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public void setDate(String stringDate) {
+        try {
+            this.date = DATE_FORMAT.parse(stringDate);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setMark(String stringMark) {
+        this.mark = Integer.parseInt(stringMark.replace(Constants.POINT, ""));
+    }
+
+    public String getStringDate() {
+        return OUT_DATE_FORMAT.format(date);
+    }
+
+    public String getStringMark() {
+        return (mark / Constants.DENOMINATOR_FOR_MARK) + Constants.POINT + (mark % Constants.DENOMINATOR_FOR_MARK);
+    }
+}
---
+++ src/constants/Constants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class Constants {
+    public static String FILENAME = "src/results.xml";
+    public static int NAME_IND = 0;
+    public static int DATE_IND = 1;
+    public static int MARK_IND = 2;
+    public static String DELIMITER = ";";
+    public static String POINT = ".";
+    public static String DATE_FORMAT = "yyyy-MM-dd";
+    public static String OUT_DATE_FORMAT = "dd.MM.yyyy";
+    public static int DENOMINATOR_FOR_MARK = 10;
+}
