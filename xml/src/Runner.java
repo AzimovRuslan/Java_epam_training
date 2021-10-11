2ffd6a66436cf07574723d99ade72810505829dc
ruslan_assimov <azimov_2001@bk.ru>
2021-10-11 19:22:25 +0300

    commit

--- xml/src/Runner.java
+++ xml/src/Runner.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Runner {
+import by.gsu.epamlab.Result;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            List<Result> results = new ArrayList<>();
+
+            parserXML parserXML = new parserXML("student");
+            parser.parse(new File("src/results.xml"), parserXML);
+    }
+
+    static class parserXML extends DefaultHandler {
+        private String el;
+        private boolean isEntered;
+
+        public parserXML(String el) {
+            this.el = el;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equals(el)) {
+                isEntered = true;
+            }
+            if(isEntered) {
+                System.out.println(String.format("Element <%s> found", qName));
+            }
+        }
+    }
 }
--- xml/src/by/gsu/epamlab/Result.java
+++ xml/src/by/gsu/epamlab/Result.java
@@ -1,2 +1,54 @@
-package by.gsu.epamlab;public class Result {
+package by.gsu.epamlab;
+
+import java.util.Date;
+
+public class Result {
+    private String login;
+    private String test;
+    private Date date;
+    private int mark;
+
+    public Result(String login, String test, Date date, int mark) {
+        this.login = login;
+        this.test = test;
+        this.date = date;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return login + ";" + test + ";" + date + ";" + mark;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
 }
