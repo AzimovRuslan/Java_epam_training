f31cfadf3612b23f6ea34f811ce0092a3c1a20dd
ruslan_assimov <azimov_2001@bk.ru>
2021-10-20 19:04:24 +0300

    jse1_1

--- jse/src/main/java/DBInitialization/DBInitializationFromXML.java
+++ jse/src/main/java/DBInitialization/DBInitializationFromXML.java
@@ -1,15 +1,15 @@
 package DBInitialization;
 
 import beans.Result;
-import org.xml.sax.helpers.DefaultHandler;
 
 import java.util.List;
 
-public class DBInitializationFromXML extends DefaultHandler {
+public class DBInitializationFromXML{
     public DBInitializationFromXML() {}
 
     public void dbInitialization(DBInitialize dbInitialize, List<Result> resultList){
         dbInitialize.delete();
+        
         for (Result result : resultList) {
             dbInitialize.createTables(result);
         }
