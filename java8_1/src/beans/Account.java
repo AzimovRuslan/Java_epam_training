8e76ada03a90e6d78c7e08456ec3a8a24ab56aef
ruslan_assimov <azimov_2001@bk.ru>
2021-10-28 18:39:59 +0300

    java8

---
+++ java8_1/src/beans/Account.java
@@ -0,0 +1,18 @@
+package beans;
+
+public class Account {
+    private String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
