d65e60de48215f66338164ba27cccd1805523cd1
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2021-10-28 18:58:17 +0300

    Add files via upload

---
+++ src/Constants/Constants.java
@@ -0,0 +1,9 @@
+package Constants;
+
+public class Constants {
+    public final static String TO_STRING_DELIMITER = "=>";
+    public final static String COLLECT_DELIMITER = ", ";
+    public final static String PASSED = "passed";
+    public final static String FAILED = "failed";
+    public final static String PASSED_COUNT = "passed count => ";
+}
