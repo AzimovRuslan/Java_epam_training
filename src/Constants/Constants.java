910efcae2d85768d004be5e172678d6f86a0a9a4
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2021-10-30 14:43:31 +0300

    Delete src directory

--- src/Constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package Constants;
-
-public class Constants {
-    public final static String TO_STRING_DELIMITER = "=>";
-    public final static String COLLECT_DELIMITER = ", ";
-    public final static String PASSED = "passed";
-    public final static String FAILED = "failed";
-    public final static String PASSED_COUNT = "passed count => ";
-}
